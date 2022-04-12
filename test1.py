1037d170d21f1af17535fa1e759891904d2d4c9c
龚羽丰 <51267806+240411562@users.noreply.github.com>
2022-04-12 15:32:16 +0800

    002
    测试

--- test1.py
+++ test1.py
@@ -1,7 +1,23 @@
-import tensorflow
+import collections
+import math
+import random
+import jieba
+import numpy as np
+import tensorflow as tf
+import os
 
+# 导入第三方库
 
-print(123456)
+# 1.从文件中提取停止词和训练文本
 
-
-# hhhhhhh
+def red_data():
+    # 读取停用词
+    stop_words = []
+    with open("data/stopwords.txt","r",encoding="UTF-8") as fStopWords:
+        line = fStopWords.readline()
+        while line:
+            stop_words.append((line[:-1])) #去\n
+            line = fStopWords.readline()
+        stop_words = set(stop_words)
+        print("停用词表读取完毕，共{n}个词".format(n=len(stop_words)))
+red_data()
